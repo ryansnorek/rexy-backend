f8cf9c7fc43d744ee853109c5bf638f04bf39f7d
Ryan <snorekr@gmail.com>
2021-12-26 10:24:18 -0500

    db

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,72 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable().unique();
+      users.string("password", 200).notNullable();
+      users.string("phone", 10).notNullable().unique();
+      users.string("email", 50).notNullable().unique();
+      users.timestamps(false, true);
     })
-}
+    .createTable("user_profile", (user_profile) => {
+      user_profile.increments("user_profile_id");
+      user_profile.string("ptype", 4);
+      user_profile.binary("uploaded_image", 255);
+      user_profile.timestamps(false, true);
+      user_profile
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+    })
+    .createTable("user_movies", (user_movies) => {
+      user_movies.increments("user_movie_id");
+      user_movies.integer("movie_id").notNullable();
+      user_movies.timestamps(false, true);
+      user_movies
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+    })
+    .createTable("user_tv_shows", (user_tv_shows) => {
+      user_tv_shows.increments("user_tv_show_id");
+      user_tv_shows.integer("tv_show_id").notNullable();
+      user_tv_shows.timestamps(false, true);
+      user_tv_shows
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+    })
+    .createTable("user_relationships", (user_relationships) => {
+      user_relationships.increments("user_relationship_id");
+      user_relationships
+        .integer("user_id")
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      user_relationships
+        .integer("other_user_id")
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("RESTRICT");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema
+    .dropTableIfExists("user_relationships")
+    .dropTableIfExists("user_tv_shows")
+    .dropTableIfExists("user_movies")
+    .dropTableIfExists("user_profile")
+    .dropTableIfExists("users");
+};
--- api/server.js
+++ api/server.js
@@ -1,29 +1,39 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const db = require("./data/db-config");
 
-function getAllUsers() { return db('users') }
+// const authRouter = require("./auth/auth-router");
+// const usersRouter = require("./users/users-router");
+// const plantsRouter = require("./plants/plants-router");
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+const server = express();
+server.use(express.static(path.join(__dirname, "../client")));
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+// server.use("/api/auth", authRouter);
+// server.use("/api/users", usersRouter);
+// server.use("/api/plants", plantsRouter);
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
+});
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+// server.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+//   });
 
-module.exports = server
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
