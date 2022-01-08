2ec6812f888ff3672ef4bb098ea68076447e8db9
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-01-08 12:48:50 -0500

    security changes

--- api/models/users-model.js
+++ api/models/users-model.js
@@ -1,15 +1,22 @@
 const db = require("../data/db-config");
 
 function getAll() {
-  return db("users");
+  return db("users")
+    .select("user_id", "username", "created_at", "updated_at");
 }
 
 function getById(user_id) {
-  return db("users").where("user_id", user_id).first();
+  return db("users")
+    .select("user_id", "username", "created_at", "updated_at")
+    .where("user_id", user_id)
+    .first();
 }
 
 function findByUsername(username) {
-  return db("users").where("username", username).first();
+  return db("users")
+    .select("user_id", "username", "password")
+    .where("username", username)
+    .first();
 }
 
 async function createUser(user) {
@@ -26,26 +33,14 @@ async function createUser(user) {
 async function updateUser(user, user_id) {
   const [updatedUser] = await db("users")
     .where("user_id", user_id)
-    .update(user, [
-        "user_id", 
-        "username", 
-        "password", 
-        "phone", 
-        "email"
-    ]);
+    .update(user, ["user_id", "username"]);
   return updatedUser;
 }
 
 async function deleteUser(user_id) {
   const [deletedUser] = await db("users")
     .where("user_id", user_id)
-    .delete([
-        "user_id", 
-        "username", 
-        "password", 
-        "phone", 
-        "email"
-    ]);
+    .delete(["user_id", "username"]);
   return deletedUser;
 }
 
