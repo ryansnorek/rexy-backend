c87139197569543934da25172a1ae1eccf405a71
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-02-26 18:10:19 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
+// const cors = require("cors");
 
 const authRouter = require("./auth/auth.router");
 const usersRouter = require("./routers/users-router");
@@ -13,10 +13,10 @@ server.use(express.json());
 server.use(helmet());
 // server.use(cors());
 
-server.use(cors({
-  origin: '*',
-  methods: ['GET','POST','DELETE','PUT']
-}));
+// server.use(cors({
+//   origin: '*',
+//   methods: ['GET','POST','DELETE','PUT']
+// }));
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
